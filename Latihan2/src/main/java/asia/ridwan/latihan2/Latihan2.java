2ca3186aa973f7d8d44df0ad4943b1ae9ff53417
ridwan <ridwan@DESKTOP-C9J541D>
2024-10-14 00:49:23 +0700

    Done

---
+++ Latihan2/src/main/java/asia/ridwan/latihan2/Latihan2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package asia.ridwan.latihan2;
+
+/**
+ *
+ * @author ridwan
+ */
+public class Latihan2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
